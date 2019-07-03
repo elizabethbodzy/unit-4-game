7fc4b5716ebb2c13e9de01f9a81032077600a101
Elizabeth Bodzy <bodze087@newschool.edu>
2019-07-02 22:02:31 -0500

    html and css for homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+$( document ).ready(function(){
+
+    //Start off by generating random number between 19 - 120
+    
+
+
+    //Appends random number amount to random-number id
+    
+
+    //generates 4 random numbers between 1 - 12 for the gems
+    
+
+    //display wins and losses to document
+
+    
+
+    //function for winning game
+
+    
+    //Click Events when tap jewels
+    
+
+
+
+
+
+    //reset game
+    
+}
